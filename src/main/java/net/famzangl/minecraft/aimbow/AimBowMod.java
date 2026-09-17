c261f39ae17a20e49c40c01a1ceb93ddf886964e
CRGBS <32331418+CRGBS@users.noreply.github.com>
2026-09-17 19:49:36 +0800

    Update AimBowMod.java

--- src/main/java/net/famzangl/minecraft/aimbow/AimBowMod.java
+++ src/main/java/net/famzangl/minecraft/aimbow/AimBowMod.java
@@ -1,19 +1,3 @@
-/*******************************************************************************
- * This file is part of Minebot.
- *
- * Minebot is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Minebot is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Minebot.  If not, see <http://www.gnu.org/licenses/>.
- *******************************************************************************/
 package net.famzangl.minecraft.aimbow;
 
 import net.famzangl.minecraft.aimbow.aiming.RayData;
@@ -41,10 +25,10 @@ import java.io.File;
 
 import static net.famzangl.minecraft.aimbow.AimbowGui.renderTrajectory;
 
-@Mod(modid="aimbow-mod", name = "AimBow", version = "0.1.0")
+@Mod(modid="AB", name = "AB", version = "0.1.0")
 public class AimBowMod {
 
-	@Instance(value = "minebot-mod")
+	@Instance(value = "AB")
 	public static AimBowMod instance;
 	public static AimbowGui gui;
 
