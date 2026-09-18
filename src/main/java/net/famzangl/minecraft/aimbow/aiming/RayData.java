2d9cbcf3e3fb7f1abd61c6a863c98c3797710b8d
CRGBS <32331418+CRGBS@users.noreply.github.com>
2026-09-18 19:38:34 +0800

    Update RayData.java

--- src/main/java/net/famzangl/minecraft/aimbow/aiming/RayData.java
+++ src/main/java/net/famzangl/minecraft/aimbow/aiming/RayData.java
@@ -5,10 +5,12 @@ import java.util.Random;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.MathHelper;
 import net.minecraft.util.Vec3;
+import java.util.ArrayList;
+
 public abstract class RayData extends TickingEntity {
-    private static final Random RANDOM=new Random(); private boolean dead; private List<Vec3> sink=Collections.emptyList();
+    private static final Random RANDOM=new Random(); private boolean dead; private List<Vec3> sink=new ArrayList<Vec3>();
     protected float prevRotationYaw,prevRotationPitch;
-    public void setTrajectorySink(List<Vec3> s){sink=s==null?Collections.<Vec3>emptyList():s;}
+    public void setTrajectorySink(List<Vec3> s) {sink = s == null ? new ArrayList<Vec3>() : s;}
     public void setLocationAndAngles(double x,double y,double z,float yaw,float pitch){rotationYaw=yaw;rotationPitch=pitch;setPosition(x,y,z);}
     public void shootFrom(Entity e){setLocationAndAngles(e.posX,e.posY+e.getEyeHeight(),e.posZ,e.rotationYaw,e.rotationPitch);shoot();}
     public void shootFromTowards(Entity e,Vec3 look){float yaw=(float)Math.toDegrees(Math.atan2(look.zCoord,look.xCoord))-90F;float pitch=(float)-Math.toDegrees(Math.atan2(look.yCoord,Math.sqrt(look.xCoord*look.xCoord+look.zCoord*look.zCoord)));setLocationAndAngles(e.posX,e.posY+e.getEyeHeight(),e.posZ,yaw,pitch);shoot();}
